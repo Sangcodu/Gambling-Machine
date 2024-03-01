b45a79eea543c724cc72961a19a806c38f1251f4
Sam Luong <sam.luong1@education.nsw.gov.au>
2024-03-01 23:32:29 +1100

    Update main.py pt.1
    Creating different function of calling deposit, getting lines and amount to bet on. After that bring them back to the main function to generate the output from the input

--- Gambling Machine Project/main.py
+++ Gambling Machine Project/main.py
@@ -1,7 +1,16 @@
+import random
 
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLONS = 3
+
+# Get the deposit amount
 def deposit():
     while True:
-        amount = input("What would you like to input (Cash/Coin): ").title()
+        amount = input("What would you like to input: ")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
@@ -11,9 +20,47 @@ def deposit():
         else:
             print("Please enter a number!")
 
-    return deposit
+    return amount
 
+# Get number of line for betting
+def get_number_of_line():
+        while True:
+            lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) +")? ")
+            if lines.isdigit():
+                lines = int(lines)
+                if 1 <= lines <= MAX_LINES:
+                    break
+                else:
+                    print("Enter a valid number of line: ")
+            else:
+                print("Please enter a number!")
+        return lines
+
+# Amount to bet on between min and max bet
+def get_bet():
+        while True:
+            amount = input("What would you like to bet on each line: ")
+            if amount.isdigit():
+                amount = int(amount)
+                if MIN_BET <= amount <= MAX_BET:
+                    break
+                else:
+                    print(f"Amount must be between ${MIN_BET} - ${MAX_BET}")
+            else:
+                print("Please enter a number!")
+        return amount
+
+# return the value from the previous function to balance and lines
+# print the amount of bet on what line and total bet
 def main():
     balance = deposit()
-    print(balance)
-main()
+    lines = get_number_of_line()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        if balance < total_bet:
+            print(f"Insufficient Fund, you current balance is {balance}")
+        else:
+            break
+    print(f"Your betting is ${bet} on {lines} line. Total bet is equal to ${total_bet}.\n")
+main() 
